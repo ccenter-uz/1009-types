b06c649c2557cb0f341bf1015f1fb5d7a790676e
Mirxonjon <mirxonjonismanov152@gmail.com>
2024-11-17 04:02:23 +0500

    sub category  swagger

--- organization/sub-category/dto/update-sub-category.dto.ts
+++ organization/sub-category/dto/update-sub-category.dto.ts
@@ -1,3 +1,4 @@
+import { SubCategoryInterfaces } from 'types/organization/sub-category';
 import {
     IsEnum,
     IsNotEmpty,
@@ -10,21 +11,29 @@ import {
   } from 'class-validator';
   import { Type } from 'class-transformer';
   import { IdDto, LanguageDto } from 'types/global';
+import { ApiProperty } from '@nestjs/swagger';
   
-  export class SubCategoryUpdateDto extends IdDto {
-
+  export class SubCategoryUpdateDto
+    extends IdDto
+    implements SubCategoryInterfaces.Update
+  {
+    @ApiProperty()
     @IsOptional()
     @IsNumber()
     categoryId?: number;
 
+    @ApiProperty()
     @IsOptional()
     @IsNumber()
     staffNumber?: number;
-  
+
+    @ApiProperty({
+      example: { ru: 'swagger-ru', uz: 'swagger-uz', cy: 'swagger-cy' },
+    })
     @IsOptional()
     @IsObject()
     @ValidateNested()
     @Type(() => LanguageDto)
-    name?: Object;
+    name: LanguageDto;
   }
   
