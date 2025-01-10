21de81eb3444789536fcfb6a7c73969f69d2d81c
Mirxonjon <mirxonjonismanov152@gmail.com>
2025-01-10 20:18:51 +0500

    fixed bug

--- user/user/dto/get-me-by-id.dto.ts
+++ user/user/dto/get-me-by-id.dto.ts
@@ -2,12 +2,10 @@ import { IsInt, IsOptional, Min } from 'class-validator';
 import { Type } from 'class-transformer';
 import { LanguageRequestDto } from '../../../global/dto/language-request.dto';
 
-export class GetMeDto extends LanguageRequestDto {
+export class GetMeDto  {
   @IsInt()
   @Min(1)
   @Type(() => Number)
   id = 1;
 
-  @IsOptional()
-  filter?: any;
 }
