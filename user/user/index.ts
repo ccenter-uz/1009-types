2dd32c272c2012d5b8295ce8b0f858c0ffea4407
imron <imronshoimov125@gmail.com>
2025-01-15 18:09:03 +0500

    refactor user get-me

--- user/user/index.ts
+++ user/user/index.ts
@@ -1,6 +1,4 @@
 export * from './commands';
 export * from './dto/create-user.dto';
-export * from './dto/get-me-by-id.dto';
 export * from './dto/update-user.dto';
 export * from './interface/user-group.interface';
-
