7afba1e30465865e0d79b800bce0d8157238f170
elshod tukhtamurodov <elshodweb@gmail.com>
2025-01-28 15:43:27 +0500

    Refactor CityRegionFilterDto and ListQueryWithOrderDto for improved structure and organization

--- organization/nearby/dto/filter-nearby.dto.ts
+++ organization/nearby/dto/filter-nearby.dto.ts
@@ -2,7 +2,7 @@ import { IsEnum, IsNumber, IsOptional } from 'class-validator';
 import { Transform, Type } from 'class-transformer';
 import { ListQueryDto, OrderEnum } from 'types/global';
 import { ApiProperty } from '@nestjs/swagger';
-import { CityRegionFilterDto } from 'types/global-filters/city-region-filter';
+import { CityRegionFilterDto } from 'types/global/dto/city-region-filter.dto';
 
 export class NearbyFilterDto extends CityRegionFilterDto {
   @ApiProperty({
@@ -19,7 +19,7 @@ export class NearbyFilterDto extends CityRegionFilterDto {
   @IsOptional()
   @Type(() => Number)
   nearbyCategoryId?: number;
-  
+
   @ApiProperty({
     enum: OrderEnum,
     required: false,
