e4f5b7718254d1e3da035fa196da5c8f3a270bf6
Mirxonjon <mirxonjonismanov152@gmail.com>
2025-04-18 19:43:55 +0500

    fixed bug

---
+++ organization/product-service-sub-category/dto/delete-product-service-sub-category.dto.ts
@@ -0,0 +1,15 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { DeleteDto, ListQueryDto } from 'types/global';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ProductServiceSubCategoryDeleteDto extends DeleteDto {
+  @ApiProperty({
+    type: String,
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  @Type(() => String)
+  deleteReason?: string;
+}
