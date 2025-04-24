f4a7b5110220e84994ff7c1107c3ea8e60099d0d
Mirxonjon <mirxonjonismanov152@gmail.com>
2025-04-24 16:50:39 +0500

    add delete query

--- organization/category/dto/delete-category.dto.ts
+++ organization/category/dto/delete-category.dto.ts
@@ -1,7 +1,7 @@
-import { IsNumber, IsOptional, IsString } from 'class-validator';
-import { Type } from 'class-transformer';
+import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
 import { DeleteDto, ListQueryDto } from 'types/global';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CategoryDeleteDto extends DeleteDto {
   @ApiProperty({
@@ -11,5 +11,22 @@ export class CategoryDeleteDto extends DeleteDto {
   @IsString()
   @IsOptional()
   @Type(() => String)
+  deleteReason?: string;
+}
+
+export class CategoryDeleteQueryDto {
+  @ApiProperty({ type: Boolean, required: true })
+  @IsOptional()
+  @IsBoolean()
+  @Transform(({ value }) => {
+    if (value === 'true' || value === true) return true;
+    if (value === 'false' || value === false) return false;
+    return undefined;
+  })
+  delete?: boolean;
+
+  @ApiPropertyOptional({ type: String })
+  @IsOptional()
+  @IsString()
   deleteReason?: string;
 }
