947811de6ae826587e7903f7d712f85ed53e2262
Mirxonjon <mirxonjonismanov152@gmail.com>
2025-08-07 04:16:15 +0500

    add types business

---
+++ organization/business/index.ts
@@ -0,0 +1,5 @@
+export * from './commands';
+export * from './dto/create-business.dto';
+export * from './dto/update-business.dto';
+export * from './interface/business-group.interface';
+export * from './dto/filter-business.dto';
