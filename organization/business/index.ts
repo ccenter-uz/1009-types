0dd431c4e5cf6504e00c2e677f504e30c708c22e
Mirxonjon <mirxonjonismanov152@gmail.com>
2025-08-08 01:35:52 +0500

    fixed b u and add new type

--- organization/business/index.ts
+++ organization/business/index.ts
@@ -3,3 +3,4 @@ export * from './dto/create-business.dto';
 export * from './dto/update-business.dto';
 export * from './interface/business-group.interface';
 export * from './dto/filter-business.dto';
+export * from './dto/filter-with-name-business.dto';
