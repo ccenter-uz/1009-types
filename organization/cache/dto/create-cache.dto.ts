3f0265fc1cd3556fedc5584e239adb434f329673
Mirxonjon <mirxonjonismanov152@gmail.com>
2025-08-21 14:37:08 +0500

    add new type

---
+++ organization/cache/dto/create-cache.dto.ts
@@ -0,0 +1,71 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
+import {
+  IsArray,
+  IsJSON,
+  IsNotEmpty,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { LanguageDto, LogDataType } from 'types/global';
+import { CacheInterfaces } from 'types/organization/cache/interface/cache-group.interface';
+import * as Multer from 'multer';
+import { PhotoLinkDto } from './file-upload-dto';
+
+export class CacheCreateDto implements CacheInterfaces.Request {
+  @IsOptional()
+  @IsNumber()
+  id: number;
+
+  @ApiProperty({
+    example: {
+      dayoffs: ['Monday'],
+      lunchFrom: '13:00',
+      lunchTo: '14:00',
+      worktimeFrom: '10:00',
+      worktimeTo: '19:00',
+    },
+  })
+  @IsOptional()
+  @IsObject()
+  @Transform(({ value }) =>
+    typeof value === 'string' ? JSON.parse(value) : value
+  )
+  data?: Record<string, string>;
+
+  @ApiProperty({
+    type: 'array',
+    items: { type: 'string', format: 'binary' },
+    required: false,
+  })
+  photos?: Array<Multer.File>;
+
+  @ApiProperty({
+    type: 'string',
+    format: 'binary',
+    required: false,
+  })
+  logo?: Multer.File;
+
+  @ApiProperty({
+    type: 'string',
+    format: 'binary',
+    required: false,
+  })
+  banner?: Multer.File;
+
+  @IsOptional()
+  @IsString()
+  logoLink?: string;
+
+  @IsOptional()
+  @IsString()
+  filesJson?: string;
+
+  @IsOptional()
+  @IsObject()
+  logData?: LogDataType;
+}
